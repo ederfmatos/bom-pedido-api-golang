e517c13ee939149b35a6ba8cc5d47d5dab62084c
Eder Matos <ederfmatos@gmail.com>
2024-07-08 19:37:12 -0300

    feature: add event consumer pool

--- presentation/messaging/product.go
+++ presentation/messaging/product.go
@@ -7,8 +7,8 @@ import (
 )
 
 func HandleProductEvents(factory *factory.ApplicationFactory) {
-	go factory.EventDispatcher.Consume("PRODUCTS::CREATE_PRODUCT_PROJECTION", func(event event.Event) error {
-		slog.Info("Handling CREATE_PRODUCT_PROJECTION event", "id", event)
-		return nil
+	factory.EventDispatcher.Consume(event.OptionsForQueue("PRODUCTS::CREATE_PRODUCT_PROJECTION"), func(message event.MessageEvent) error {
+		slog.Info("Received product message", "message", message.AsEvent())
+		return message.Ack()
 	})
 }
