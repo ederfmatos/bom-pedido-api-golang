85f985a04c42cc519d195488d1c1b83b630feab4
Eder Matos <ederfmatos@gmail.com>
2024-08-29 16:06:34 -0300

    refactor: move google gateway to separated folder

--- infra/factory/gateway.go
+++ infra/factory/gateway.go
@@ -3,7 +3,7 @@ package factory
 import (
 	"bom-pedido-api/application/factory"
 	"bom-pedido-api/infra/config"
-	"bom-pedido-api/infra/gateway"
+	"bom-pedido-api/infra/gateway/google"
 	"bom-pedido-api/infra/gateway/pix"
 	"bom-pedido-api/infra/http_client"
 	"bom-pedido-api/infra/repository"
@@ -12,7 +12,7 @@ import (
 func gatewayFactory(environment *config.Environment, connection repository.SqlConnection) *factory.GatewayFactory {
 	paymentGatewayConfigRepository := repository.NewDefaultMerchantPaymentGatewayConfigRepository(connection)
 	return factory.NewGatewayFactory(
-		gateway.NewDefaultGoogleGateway(http_client.NewDefaultHttpClient(environment.GoogleAuthUrl)),
+		google.NewDefaultGoogleGateway(http_client.NewDefaultHttpClient(environment.GoogleAuthUrl)),
 		pix.NewLogPixGatewayDecorator(
 			pix.NewWooviPixGateway(
 				environment.PixPaymentGateway.ExpirationTimeInMinutes,
